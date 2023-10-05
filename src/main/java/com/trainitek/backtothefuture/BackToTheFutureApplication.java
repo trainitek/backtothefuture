377ce4617cb5550a692c6dd5834d35934525acfb
Marek Dominiak <marek.dominiak@mintra.com>
2023-10-05 20:43:24 +0200

    barebones

---
+++ src/main/java/com/trainitek/backtothefuture/BackToTheFutureApplication.java
@@ -0,0 +1,13 @@
+package com.trainitek.backtothefuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToTheFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackToTheFutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainitek/backtothefuture/BackToTheFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainitek.backtothefuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackToTheFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
