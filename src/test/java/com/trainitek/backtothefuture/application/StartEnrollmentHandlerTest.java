ffe2541133efbcbc7cdc57e2fc065d311a073d2b
Marek Dominiak <marek.dominiak@mintra.com>
2023-10-17 14:21:26 +0200

    Improved some tests

--- src/test/java/com/trainitek/backtothefuture/application/StartEnrollmentHandlerTest.java
+++ src/test/java/com/trainitek/backtothefuture/application/StartEnrollmentHandlerTest.java
@@ -2,13 +2,13 @@ package com.trainitek.backtothefuture.application;
 
 import com.trainitek.backtothefuture.domain.*;
 import com.trainitek.backtothefuture.test.fixtures.Fixtures;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import spock.util.time.MutableClock;
 
+import java.time.Duration;
 import java.time.Instant;
 
 import static java.time.Duration.ofDays;
@@ -46,23 +46,6 @@ class StartEnrollmentHandlerTest {
     }
 
     @Test
-    void shouldCreateASimpleEnrollmentAndStartIt() {
-        // given
-        Instant enrolledAt = Instant.now(clock);
-        Instant availableFrom = enrolledAt.plus(ofDays(1));
-        Enrollment enrollment = Enrollment.initialEnrollment(student, student, course, enrolledAt, availableFrom);
-        enrollmentRepository.save(enrollment);
-
-        // when
-        clock.adjust(adjuster -> adjuster.plus(ofDays(2)));
-        handler.start(enrollment.getId());
-
-        // then
-        assertThat(enrollmentRepository.findById(enrollment.getId()))
-                .hasValueSatisfying(Enrollment::isStarted);
-    }
-
-    @Test
     void shouldNotAllowToStartEnrollmentTooEarly() {
         // given
         Instant enrolledAt = Instant.now(clock);
@@ -72,10 +55,31 @@ class StartEnrollmentHandlerTest {
 
         // when
         assertThatThrownBy(() -> handler.start(enrollment.getId()))
-                        .hasMessageContaining("Cannot start the enrollment");
+                .hasMessageContaining("Cannot start the enrollment");
 
         // then
         assertThat(enrollmentRepository.findById(enrollment.getId()))
                 .hasValueSatisfying(Enrollment::isStarted);
+    }
+
+    @Test
+    void shouldCreateASimpleEnrollmentAndStartIt() {
+        // given
+        Instant enrolledAt = Instant.now(clock);
+        Instant availableFrom = enrolledAt.plus(ofDays(1));
+        Enrollment enrollment = Enrollment.initialEnrollment(student, student, course, enrolledAt, availableFrom);
+        enrollmentRepository.save(enrollment);
+
+        // when
+        moveToFutureBy(ofDays(2));
+        handler.start(enrollment.getId());
+
+        // then
+        assertThat(enrollmentRepository.findById(enrollment.getId()))
+                .hasValueSatisfying(Enrollment::isStarted);
+    }
+
+    private void moveToFutureBy(Duration amount) {
+        clock.adjust(adjuster -> adjuster.plus(amount));
     }
 }
